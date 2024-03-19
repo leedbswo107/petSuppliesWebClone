6f59630e24e3284edbec9e7e7e5845013c7f500e
leedbswo107 <ljyu1@naver.com>
2024-03-19 23:29:02 +0900

    feat: slide func js upload

---
+++ js/common.js
@@ -0,0 +1,27 @@
+let productSlide = new Swiper('.productSlide', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.next1',
+    prevEl: '.prev1',
+  },
+});
+
+let reviewSlide = new Swiper('.reviewSlide', {
+  navigation: {
+    nextEl: '.next2',
+    prevEl: '.prev2',
+  },
+});
+let brandCommentSlide = new Swiper('.brandCommentSlide', {
+  navigation: {
+    nextEl: '.next3',
+    prevEl: '.prev3',
+  },
+});
+let designProductSlide = new Swiper('.designProductSlide', {
+  navigation: {
+    nextEl: '.next4',
+    prevEl: '.prev4',
+  },
+});
